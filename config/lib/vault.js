66e22ba8a72900e4e1f844ea17aa2521ecd3bf01
Liran Tal <liran.tal@gmail.com>
2016-06-29 12:00:43 +0300

    fix(vault): temporary file download/directory change, also introduced debug package use for that part

--- config/lib/vault.js
+++ config/lib/vault.js
@@ -1,9 +1,17 @@
 'use strict';
 
 var fs = require('fs');
+var crypto = require('crypto');
 var clamav = require('clamav.js');
 var request = require('request');
 var Q = require('q');
+var debug = require('debug')('vault');
+
+/**
+ * vault configuration parameter for a writable temporary directory
+ * @type {String}
+ */
+var tmpDirectory = "c:\Temp";
 
 var clamavScanner = clamav.createScanner(3310, '127.0.0.1');
 
@@ -41,17 +49,23 @@ function download(vault) {
   // @TODO check if the path exists and if it is accessible
   // @TODO make this path for local download configurable
   // @TODO save the downloaded file using a generated random hash
-  var localFile = '/tmp/file.jpg';
+  var localFile = tmpDirectory + getRandomString();
+  debug('downloader: temporary directory set to: %s', tmpDirectory);
+  if (isPathWritable(tmpDirectory) !== true) {
+    deferred.reject(new Error('"unable to write to directory'));
+  }
+
   var file = fs.createWriteStream(localFile);
 
-  var requestUrl = composeRequestObject(vault.url);
+  var requestUrl = getRequestObject(vault.url);
 
   vault.local.tmpFile = localFile;
 
-  console.log('-> download: starting to download');
+  debug('downloader: starting to download: %s', vault.url);
   request.get(requestUrl)
     .on('error', function(err) {
-      console.log('-> download: request error:');
+      debug('downloader: request error for file %s', vault.url);
+      debug(err);
       vault.local.status = 'err';
       vault.local.msg = err;
       deferred.reject(vault);
@@ -59,7 +73,8 @@ function download(vault) {
     .pipe(file);
 
   file.on('error', function(err) {
-    console.log('-> download: file on error');
+    debug('downloader: unable to process file: %s', vault.url);
+    debug(err);
     vault.local.status = 'err';
     vault.local.msg = err;
     deferred.reject(vault);
@@ -67,7 +82,7 @@ function download(vault) {
 
   file.on('finish', function() {
     file.close(function() {
-      console.log('-> download: file closed:');
+      debug('downloader: successfully downloaded file: %s', vault.url);
       vault.local.status = 'ok';
       deferred.resolve(vault);
     });
@@ -85,7 +100,6 @@ function scanFile(vault) {
 
   var deferred = Q.defer();
 
-  // @TODO update all of console.log childish things with the debug package
   console.log('-> scanFile');
   console.log(vault);
 
@@ -110,12 +124,41 @@ function scanFile(vault) {
   return deferred.promise;
 }
 
+
+/**
+ * checks if a given path is writable
+ * @param  {string}  directory path to check if writable or not by the running nodejs process
+ * @return {Boolean}           [description]
+ */
+function isPathWritable(directory) {
+
+  var success = true;
+
+  try {
+    fs.accessSync(directory, fs.W_OK);
+  } catch (err) {
+    success = false;
+  }
+
+  return success;
+}
+
+/**
+ * creates a random string that can be used to save the file in the temporary directory
+ * @return {string} randomly generated string
+ */
+function getRandomString() {
+  // @TODO upadte this method to asynchronous random string generation which
+  // also requires to update the flow in the calling method
+  return crypto.randomBytes(48).toString('hex');
+}
+
 /**
  * given a url, create a request-compatible object (request - the npm library)
  * @param  {string} url url of a file to download
  * @return {object}     returns a promise object
  */
-function composeRequestObject(url) {
+function getRequestObject(url) {
 
   var obj = {};
 
