a48259443d332d8d430e58d0574afeecebeb8401
Liran Tal <liran.tal@gmail.com>
2016-08-22 00:09:17 +0300

    adding a simple route as an example for ping URL callback

--- modules/vault/server/routes/vault.server.routes.js
+++ modules/vault/server/routes/vault.server.routes.js
@@ -11,4 +11,11 @@ var vault = require('../controller/vault.server.controller');
  */
 module.exports = function(app) {
   app.route('/api/vaultQueue').post(vault.saveVaultDate);
+
+  // comment out for testing the ping back URL from vault
+  // app.route('/api/test').all(function(req, res, next) {
+  //   console.log('vault server pinged back with:');
+  //   console.log(req.body);
+  //   next();
+  // });
 };
